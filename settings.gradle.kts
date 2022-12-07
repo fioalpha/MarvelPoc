b3dcc805b771a9147635e41c8a491563dfddd0de
fioalpha <fioalpha@gmail.com>
2022-12-07 20:09:41 -0300

    add sharing depenedency from multiple module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,16 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+//    versionCatalogs {
+//        create("libs") {
+//            from(files("gradle/libs.versions.toml"))
+//        }
+//    }
 }
 rootProject.name="Marvel"
 include(
     ":app",
     ":platform:core:network"
 )
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
