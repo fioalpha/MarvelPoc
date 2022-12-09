4901839a90f5f6fa768c2fb194344f7a8a1f18d1
fioalpha <fioalpha@gmail.com>
2022-12-09 20:48:29 -0300

    remove commentaties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,13 +11,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-//    versionCatalogs {
-//        create("libs") {
-//            from(files("gradle/libs.versions.toml"))
-//        }
-//    }
-
-
 }
 rootProject.name = "Marvel"
 include(
