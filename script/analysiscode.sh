fe6a99ff4dd44b8b6848650a4f162fab7f07bbe5
fioalpha <fioalpha@gmail.com>
2022-12-21 21:58:58 -0300

    reformated code

--- script/analysiscode.sh
+++ script/analysiscode.sh
@@ -39,7 +39,7 @@ run_detekt() {
     echo "$?"
     if test "$?" == 0; then
       echo " asdklhjfklsdf"
-        exit 0
+        exit 1
     fi
     echo -e "${green_color}No issues found with Detekt${normal_color}"
 }
